a7068764e9208006e0abaea466582468c7fe285f
Marius Landwehr <marius.landwehr@gmail.com>
2015-12-21 21:49:10 +0100

    Added CocoaDoc

--- Classes/HexColors.h
+++ Classes/HexColors.h
@@ -24,7 +24,22 @@
 
 @interface HXColor (HexColorAddition)
 
+/** 
+ Creates a UIColor or NSColor from a HexString like #fff, #ffff, #ff00aa or #ff00aaee.
+ With complete support for short hand hex values, short hand with short alpha value, full 
+ hex values and full hex values with alpha.
+ 
+ @param hexString NSString hex string for color generation
+ @return UIColor / NSColor or nil
+ */
 + (nullable HXColor *)hx_colorWithHexString:(nonnull NSString *)hexString;
+/**
+ Same implementation as hx_colorWithHexString but you can hand in a normal alpha value from 0 to 1
+ 
+ @param hexString NSString hex string for color generation
+ @param alpha CGFloat from 0 to 1
+ @return UIColor / NSColor or nil
+ */
 + (nullable HXColor *)hx_colorWithHexString:(nonnull NSString *)hexString alpha:(CGFloat)alpha;
 
 + (nonnull HXColor *)hx_colorWith8BitRed:(NSInteger)red green:(NSInteger)green blue:(NSInteger)blue;
