212c3ae8ccd7057e1f8070d4988fcd4b0296d293
Alexander Kraev <kraev_as@utkonos.ru>
2022-09-01 19:02:07 +0300

    add SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.6
 //
 //  HexColors.swift
 //
@@ -15,5 +16,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "HexColors"
+     name: "HexColors",
+     platforms: [
+         .iOS(.v11),
+         .macOS(.v10_15),
+         .tvOS(.v13)
+     ],
+     products: [
+         .library(
+             name: "HexColors",
+             targets: ["HexColors"]),
+         .library(
+             name: "HexColors-Static",
+             type: .static,
+             targets: ["HexColors"]),
+         .library(
+             name: "HexColors-Dynamic",
+             type: .dynamic,
+             targets: ["HexColors"]),
+     ],
+     dependencies: [],
+     targets: [
+         .target(
+             name: "HexColors",
+             dependencies: [],
+             path: "Sources"
+         )
+     ]
 )
